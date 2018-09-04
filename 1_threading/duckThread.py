b263b768503ec0a2cfa1e01a0f37512ac16cbc22
Worawan Diaz Carballo <wdc@cs.tu.ac.th>
2018-09-04 20:53:53 +0700

    adding lock example

--- 1_threading/duckThread.py
+++ 1_threading/duckThread.py
@@ -2,20 +2,22 @@ import threading
 
 class Duck(threading.Thread):
     times = 1
-    def __init__(self, n):
+    def __init__(self, name, n):
          threading.Thread.__init__(self)
+         self.name = name
          self.times = n
 
     def run(self):
         for i in range(self.times):
-            print('quack quack')
+            print(f'{self.name}: quack quack')
 
 def main():
     numDucks = 4
+    numQuacks = 3
     ducks = []
 
     for i in range(numDucks):
-        duck1 = Duck(3)
+        duck1 = Duck(f'Duck {i}', numQuacks)
         ducks.append(duck1)
 
     print(f'we have {len(ducks)} ducks')
