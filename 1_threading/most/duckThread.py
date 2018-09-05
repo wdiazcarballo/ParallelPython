1a18f5d56123f220f56872bb5c3c76facc463734
Thanakorn Temee <>
2018-09-05 12:10:36 +0700

    Duck Thread with lock

--- 1_threading/most/duckThread.py
+++ 1_threading/most/duckThread.py
@@ -1,31 +1,32 @@
 import threading
+import time
 
 class Duck(threading.Thread):
-    times = 1
-    def __init__(self, name, n):
-         threading.Thread.__init__(self)
-         self.name = name
-         self.times = n
-
+    walking = 'Walks like a duck.'
+    def __init__(self, quack , num):
+        threading.Thread.__init__(self)
+        self.quack = quack
+        self.num = num
+        print(f'Time to quack = {self.quack}')
     def run(self):
-        for i in range(self.times):
-            print(f'{self.name}: quack quack')
+        for i in range(self.quack):
+            print(f'Duck Number : {self.num} Quaaaack Quaaaack!!')
+
+
 
 def main():
     numDucks = 4
-    numQuacks = 3
+    Quaaaack = 3
     ducks = []
+    for i in range(numDucks): 
+        ducks.append(Duck(Quaaaack,i))
+        # print(len(ducks))
 
-    for i in range(numDucks):
-        duck1 = Duck(f'Duck {i}', numQuacks)
-        ducks.append(duck1)
 
-    print(f'we have {len(ducks)} ducks')
-    
     for i in range(len(ducks)):
         ducks[i].start()
         ducks[i].join()
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == '__main__' : main()
+
